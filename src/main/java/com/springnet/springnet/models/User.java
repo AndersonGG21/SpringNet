bb5f2e074e2b0069a4b2e3df48027ff95d6ce8a1
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-11 22:43:42 -0500

    Add user model

---
+++ src/main/java/com/springnet/springnet/models/User.java
@@ -0,0 +1,37 @@
+package com.springnet.springnet.models;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.ToString;
+
+@Entity
+@Table(name = "users")
+@ToString
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "date")
+    private LocalDateTime registrationDate;
+
+    @Column(name = "description")
+    private String description;
+}
