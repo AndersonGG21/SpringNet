ff63d04cb6a2415aa653221225c3a3fa4ebda355
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-12 14:05:38 -0500

    Add Like model

---
+++ src/main/java/com/springnet/springnet/models/Like.java
@@ -0,0 +1,33 @@
+package com.springnet.springnet.models;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "likes")
+@Data
+public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "date")
+    private LocalDateTime date;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+}
