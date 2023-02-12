69828432453f8a831224e72cd7a368f54529dc40
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-12 17:37:30 -0500

    Add find user by id in PostRepo

--- src/main/java/com/springnet/springnet/repositories/PostRepository.java
+++ src/main/java/com/springnet/springnet/repositories/PostRepository.java
@@ -5,9 +5,8 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.springnet.springnet.models.Post;
-import com.springnet.springnet.models.User;
 
 public interface PostRepository extends JpaRepository<Post, Long>{
     
-    List<Post> findPostByUserIn(List<User> users);
+    List<Post> findByUserId(Long userId);
 }
