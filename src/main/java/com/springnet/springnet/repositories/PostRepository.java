a6f274dc62eb7826d8a9d5bbe256b77025a797de
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-12 16:50:50 -0500

    Add PostRespository

---
+++ src/main/java/com/springnet/springnet/repositories/PostRepository.java
@@ -0,0 +1,13 @@
+package com.springnet.springnet.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springnet.springnet.models.Post;
+import com.springnet.springnet.models.User;
+
+public interface PostRepository extends JpaRepository<Post, Long>{
+    
+    List<Post> findPostByUserIn(List<User> users);
+}
