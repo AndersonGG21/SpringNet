3a7e1d32665f509d616067b2bb653a6b9b3e81c4
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-12 13:45:27 -0500

    Add Post model

---
+++ src/main/java/com/springnet/springnet/models/Post.java
@@ -0,0 +1,12 @@
+package com.springnet.springnet.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "posts")
+@Data
+public class Post {
+    
+}
