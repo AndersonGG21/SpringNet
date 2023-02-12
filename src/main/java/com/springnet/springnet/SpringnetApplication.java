0c052c2e20324c3794072660ee9c8b6f108825f3
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-11 22:43:37 -0500

    Initialize project

---
+++ src/main/java/com/springnet/springnet/SpringnetApplication.java
@@ -0,0 +1,14 @@
+package com.springnet.springnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringnetApplication.class, args);
+		System.out.println("SpringNet works!");
+	}
+
+}
---
+++ src/test/java/com/springnet/springnet/SpringnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springnet.springnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
