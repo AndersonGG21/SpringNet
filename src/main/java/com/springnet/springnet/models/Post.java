6b8e00f433ac273d7c60df87ab40f18479adc5f9
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-12 13:57:51 -0500

    Add Post atributes

---
+++ src/main/java/com/springnet/springnet/models/Post.java
@@ -0,0 +1,37 @@
+package com.springnet.springnet.models;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "posts")
+@Data
+public class Post {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "image")
+    private String image;
+
+    @Column(name = "date")
+    private LocalDateTime publicationDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
