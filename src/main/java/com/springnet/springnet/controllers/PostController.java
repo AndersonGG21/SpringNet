5b5333ee832933913d2f5ff9482c8740f1970204
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-12 17:55:27 -0500

    Add endpoint to create a new post

--- src/main/java/com/springnet/springnet/controllers/PostController.java
+++ src/main/java/com/springnet/springnet/controllers/PostController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +24,10 @@ public class PostController {
     public List<Post> getPostByFollowing(@PathVariable Long userId){
         return postService.findPostByFollowing(userId);
     }
+
+    @PostMapping("/new-post")
+    public void createPost(@RequestBody Post post){
+        postService.createPost(post);
+    }
+
 }
