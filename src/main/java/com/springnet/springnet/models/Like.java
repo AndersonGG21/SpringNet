6b64cc9521b4298f382fb805e997ecc244e6a1be
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-13 12:24:57 -0500

    Remove unused imports

--- src/main/java/com/springnet/springnet/models/Like.java
+++ src/main/java/com/springnet/springnet/models/Like.java
@@ -1,8 +1,5 @@
 package com.springnet.springnet.models;
 
-import java.time.LocalDateTime;
-
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
