77d30dc612274cac86c75fbb008ec35833b76214
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-13 12:07:52 -0500

    Remove attribute

--- src/main/java/com/springnet/springnet/models/Like.java
+++ src/main/java/com/springnet/springnet/models/Like.java
@@ -20,9 +20,6 @@ public class Like {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "date")
-    private LocalDateTime date;
-
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
