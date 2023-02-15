5f35592da41d1f7460a4e0b9f1988ff01bcf8a77
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-15 11:19:46 -0500

    Create UserController

---
+++ src/main/java/com/springnet/springnet/controllers/UserController.java
@@ -0,0 +1,21 @@
+package com.springnet.springnet.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springnet.springnet.services.UserServiceImpl;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    
+    @Autowired
+    private UserServiceImpl userService;
+
+
+    
+    
+}
