96eeaaf7ee3d94125535c6d5c117de08dfcb8ddc
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-15 11:22:19 -0500

    Add create a new user method

--- src/main/java/com/springnet/springnet/controllers/UserController.java
+++ src/main/java/com/springnet/springnet/controllers/UserController.java
@@ -1,7 +1,7 @@
 package com.springnet.springnet.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +21,8 @@ public class UserController {
         return userService.getUserProfile(id);
     }
     
-    
-    
+    @PostMapping("/new-user")
+    public void createUser(@RequestBody User newUser){
+        userService.createUser(newUser);
+    }    
 }
