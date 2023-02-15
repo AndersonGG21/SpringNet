78aaf024ac254c8f7a080aeb9be29427c8231ee2
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-15 11:13:19 -0500

    Add get user profile function

--- src/main/java/com/springnet/springnet/services/UserService.java
+++ src/main/java/com/springnet/springnet/services/UserService.java
@@ -1,9 +1,7 @@
 package com.springnet.springnet.services;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.springnet.springnet.models.User;
 
 public interface UserService{
-    User findUserById(Long id);
+    User getUserProfile(Long id);
 }
--- src/main/java/com/springnet/springnet/services/UserServiceImpl.java
+++ src/main/java/com/springnet/springnet/services/UserServiceImpl.java
@@ -1,16 +1,20 @@
 package com.springnet.springnet.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.springnet.springnet.models.User;
+import com.springnet.springnet.repositories.UserRepository;
 
 @Service
 public class UserServiceImpl implements UserService{
 
+    @Autowired
+    private UserRepository userRepo;
+
     @Override
-    public User findUserById(Long id) {
-        // TODO Auto-generated method stub
-        return null;
+    public User getUserProfile(Long id) {
+        return userRepo.findById(id).orElse(null);
     }
     
 }
