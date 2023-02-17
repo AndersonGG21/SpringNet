bfb24a626a8150f80df44a4f3ceca914d1b305ff
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-16 21:00:08 -0500

    Add getAuthentication

--- src/main/java/com/springnet/springnet/security/TokenUtils.java
+++ src/main/java/com/springnet/springnet/security/TokenUtils.java
@@ -1,9 +1,14 @@
 package com.springnet.springnet.security;
 
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
@@ -21,5 +26,20 @@ public class TokenUtils {
 
         return Jwts.builder().setSubject(email).setExpiration(expirationDate).addClaims(extra)
                 .signWith(Keys.hmacShaKeyFor(ACCESS_TOKEN_SECRET.getBytes())).compact();
+    }
+
+    public static UsernamePasswordAuthenticationToken getAuthentication(String token) {
+
+        try {
+            Claims claims = Jwts.parserBuilder().setSigningKey(ACCESS_TOKEN_SECRET.getBytes()).build().parseClaimsJws(token).getBody();
+
+            String email = claims.getSubject();
+
+            return new UsernamePasswordAuthenticationToken(email, null, Collections.emptyList());
+        } catch (JwtException e) {
+            return null;
+        }
+
+        
     }
 }
