35592b806bd8eaaf536635aef59b37f829220044
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-16 20:40:37 -0500

    Add password encoder

--- src/main/java/com/springnet/springnet/security/WebSecurityConfig.java
+++ src/main/java/com/springnet/springnet/security/WebSecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -40,5 +42,10 @@ public class WebSecurityConfig {
         );
 
         return manager;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
