8fea9061511d740e4f59f91ba244714cc8794cf7
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-16 20:39:14 -0500

    Add security filter chain method

---
+++ src/main/java/com/springnet/springnet/security/WebSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.springnet.springnet.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authManager) throws Exception{
+        return http.
+        csrf()
+        .disable()
+        .authorizeHttpRequests()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .httpBasic()
+        .and()
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().build();
+    }
+
+}
