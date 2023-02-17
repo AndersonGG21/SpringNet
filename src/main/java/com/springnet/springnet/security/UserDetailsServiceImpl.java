786a7a9dfe5c496646e9562ebea8627254a799bd
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-16 21:16:21 -0500

    Complete loadUserByUsername

--- src/main/java/com/springnet/springnet/security/UserDetailsServiceImpl.java
+++ src/main/java/com/springnet/springnet/security/UserDetailsServiceImpl.java
@@ -2,22 +2,23 @@ package com.springnet.springnet.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsPasswordService;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import com.springnet.springnet.models.User;
 import com.springnet.springnet.repositories.UserRepository;
 
-public class UserDetailsServiceImpl implements UserDetailsService{
+public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepo;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return null;
+        User user = userRepo.findOneByEmail(email)
+                    .orElseThrow(() -> new UsernameNotFoundException("El usuario con el email: " + email + " no existe."));
+                    
+        return new UserDetailsImpl(user);
     }
 
-    
-    
 }
