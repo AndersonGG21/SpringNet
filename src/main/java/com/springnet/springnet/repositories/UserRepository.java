c4deb4b3be73de8e72c8afef37bbff1317731cbb
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-16 21:06:32 -0500

    Creat find by email method

--- src/main/java/com/springnet/springnet/repositories/UserRepository.java
+++ src/main/java/com/springnet/springnet/repositories/UserRepository.java
@@ -1,8 +1,11 @@
 package com.springnet.springnet.repositories;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.springnet.springnet.models.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
     
+    Optional<User> findOneByEmail(String email);
 }
