516ececfdbf4ff73282ec09ac3607a0ddf182345
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-28 22:59:40 -0500

    Create Follow Controller

---
+++ src/main/java/com/springnet/springnet/controllers/FollowController.java
@@ -0,0 +1,11 @@
+package com.springnet.springnet.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/follows")
+public class FollowController {
+    
+
+}
