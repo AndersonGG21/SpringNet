83105f1aa2c457e05fc22715ed1a0c94d1bedf0e
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-28 22:38:06 -0500

    Remove native query

--- src/main/java/com/springnet/springnet/repositories/PostRepository.java
+++ src/main/java/com/springnet/springnet/repositories/PostRepository.java
@@ -3,14 +3,10 @@ package com.springnet.springnet.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.springnet.springnet.models.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long>{
     
     List<Post> findByUserId(Long userId);
-
-    @Query ("SELECT COUNT(follower_id) FROM `follow` WHERE following_id = 12")
-    List<Object> countFollowers();
 }
