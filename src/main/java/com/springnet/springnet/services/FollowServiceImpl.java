dfe1197e0416e65c853b703678d97368a0d3eb7a
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-28 22:56:26 -0500

    refactor: Change get count of followers method to another service

--- src/main/java/com/springnet/springnet/services/FollowServiceImpl.java
+++ src/main/java/com/springnet/springnet/services/FollowServiceImpl.java
@@ -1,11 +1,19 @@
 package com.springnet.springnet.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import jakarta.persistence.EntityManager;
+
+@Service
 public class FollowServiceImpl implements FollowService{
+
+    @Autowired
+    private EntityManager em;
 
     @Override
     public Long getCountOfFollowers() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getCountOfFollowers'");
+        return (Long) em.createNativeQuery("SELECT COUNT(follower_id) FROM `follow` WHERE following_id = 12").getSingleResult();
     }
 
     @Override
