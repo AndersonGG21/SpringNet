850a064535b04e2fa50addc7e270aa852c85b56e
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-02-28 23:01:23 -0500

    Add endpoint to get count of followers

--- src/main/java/com/springnet/springnet/controllers/FollowController.java
+++ src/main/java/com/springnet/springnet/controllers/FollowController.java
@@ -1,11 +1,21 @@
 package com.springnet.springnet.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.springnet.springnet.services.FollowService;
 
 @RestController
 @RequestMapping("/api/follows")
 public class FollowController {
     
+    @Autowired
+    private FollowService followService;
 
+    @GetMapping("/count-followers")
+    public Long getCountOfFollowers(){
+        return followService.getCountOfFollowers();
+    }
 }
