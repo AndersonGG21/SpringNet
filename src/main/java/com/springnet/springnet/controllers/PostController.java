b3733021e7a4330ee3ddd88c2db0ad950fe27968
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-03-11 22:05:40 -0500

    Add method to check if a like exists

--- src/main/java/com/springnet/springnet/controllers/PostController.java
+++ src/main/java/com/springnet/springnet/controllers/PostController.java
@@ -56,4 +56,8 @@ public class PostController {
         return postService.findPostByUser(user);
     }
 
+    @PostMapping("/liked")
+    public Long checkLike(@RequestBody Like like){
+        return postService.countLike(like);
+    }
 }
