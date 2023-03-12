adbf47026375270389eae6f20963557bdba0dee2
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-03-11 22:06:20 -0500

    Format code

--- src/main/java/com/springnet/springnet/repositories/LikeRepository.java
+++ src/main/java/com/springnet/springnet/repositories/LikeRepository.java
@@ -1,5 +1,6 @@
 package com.springnet.springnet.repositories;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.springnet.springnet.models.Like;
@@ -7,8 +8,5 @@ import com.springnet.springnet.models.Post;
 import com.springnet.springnet.models.User;
 
 public interface LikeRepository extends JpaRepository<Like, Long>{
-
     Like findByPostAndUser(Post post, User user);
-    
-    
 }
