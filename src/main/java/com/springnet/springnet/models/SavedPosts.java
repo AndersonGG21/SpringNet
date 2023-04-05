fdb29c04db17c35d6c2001377c42f3c7f32dc0c4
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-04-04 20:25:51 -0500

    Add attributes to SavedPosts model

--- src/main/java/com/springnet/springnet/models/SavedPosts.java
+++ src/main/java/com/springnet/springnet/models/SavedPosts.java
@@ -1,5 +1,27 @@
 package com.springnet.springnet.models;
 
+import lombok.Data;
+import jakarta.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+@Table(name = "saved_posts")
+@Data
 public class SavedPosts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
 }
