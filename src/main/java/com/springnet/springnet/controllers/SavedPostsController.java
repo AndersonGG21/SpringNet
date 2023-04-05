5dc7f96e327c5df9e03bce724463478edb490ffe
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-04-04 20:25:34 -0500

    Create SavedPosts controller

---
+++ src/main/java/com/springnet/springnet/controllers/SavedPostsController.java
@@ -0,0 +1,32 @@
+package com.springnet.springnet.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springnet.springnet.models.SavedPosts;
+import com.springnet.springnet.services.SavedPostsService;
+
+@RestController
+@RequestMapping("/api/saved-posts")
+public class SavedPostsController {
+    
+    @Autowired
+    private SavedPostsService savedPostsService;
+
+    @GetMapping("/{userId}")
+    public List<SavedPosts> getSavedPosts(@PathVariable Long userId){
+        return savedPostsService.getSavedPostsByUserId(userId);
+    }
+
+    @PostMapping("/save-post")
+    public void savePost(@RequestBody SavedPosts post){
+        savedPostsService.savePost(post);
+    }
+}
