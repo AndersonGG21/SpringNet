b8aee09c75fb732d4bc2792995f4d271d82302c0
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-04-06 20:35:58 -0500

    Add StoryView model

---
+++ src/main/java/com/springnet/springnet/models/StoryView.java
@@ -0,0 +1,29 @@
+package com.springnet.springnet.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "story_views")
+@Data
+public class StoryView {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "story_id")
+    private Story story;
+
+}
