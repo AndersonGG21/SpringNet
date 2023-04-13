80680e483ffc5f90b31a1ed6a4f46f6f2a6f5f50
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-04-13 16:48:19 -0500

    Remove unused imports

--- src/main/java/com/springnet/springnet/models/Story.java
+++ src/main/java/com/springnet/springnet/models/Story.java
@@ -1,8 +1,5 @@
 package com.springnet.springnet.models;
 
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,7 +7,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
