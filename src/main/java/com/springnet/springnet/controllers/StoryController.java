87724fd1dfbe63e81d4b09585d98a64fe7e1c807
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-06-22 15:19:04 -0500

    Update field injections

--- src/main/java/com/springnet/springnet/controllers/StoryController.java
+++ src/main/java/com/springnet/springnet/controllers/StoryController.java
@@ -11,13 +11,17 @@ import com.springnet.springnet.services.StoryService;
 @RestController
 @RequestMapping("/api/stories")
 public class StoryController {
-    
-    @Autowired
-    private StoryService storyService;
+
+
+    private final StoryService storyService;
+
+    public StoryController(StoryService storyService) {
+        this.storyService = storyService;
+    }
 
     @GetMapping("/{userId}")
     public List<Story> getStoriesByUserIdAndNotViewed(@PathVariable Long userId){
-        return storyService.getStoriesByUserIdAndNotViewed(userId);
+        return storyService.getStoriesByFollowing(userId);
     }
     @PostMapping("/new-story")
     public void addStory(@RequestBody Story story) {
