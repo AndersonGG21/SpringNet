0a2ce00fa81e38b3e16695b65c5b1fc403fdd4e6
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-06-22 16:24:36 -0500

    Remove unused querys

--- src/main/java/com/springnet/springnet/repositories/StoryRepository.java
+++ src/main/java/com/springnet/springnet/repositories/StoryRepository.java
@@ -9,10 +9,5 @@ import org.springframework.data.repository.query.Param;
 import com.springnet.springnet.models.Story;
 
 public interface StoryRepository extends JpaRepository<Story, Long >{
-    // List<Story> findByUserId(Long userId);
-
-    @Query("SELECT s FROM Story s JOIN FETCH s.follow f WHERE f.follower.id = :followerId")
-    List<Story> findByRelationFollowerId(@Param("followerId") Long followerId);
-
     List<Story> findByUserId(Long userId);
 }
