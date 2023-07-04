a067fa1a031a167d4215e0405baa2da94290f2c4
AndersonGG21 <andersongarcesgarcia@gmail.com>
2023-07-04 18:14:56 -0500

    Add new endpoint for websocket connection

--- src/main/java/com/springnet/springnet/controllers/WebSocketController.java
+++ src/main/java/com/springnet/springnet/controllers/WebSocketController.java
@@ -1,25 +1,38 @@
 package com.springnet.springnet.controllers;
 
 import com.springnet.springnet.models.ChatMessage;
+import com.springnet.springnet.models.MessageType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import java.util.ArrayList;
+
 
 @Controller
 public class WebSocketController {
+    private final SimpMessagingTemplate messagingTemplate;
 
-    @MessageMapping("/chat.senMessage")
-    @SendTo("/topic/public")
+    public WebSocketController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    @MessageMapping("/chat.onlineUsers")
+    //@SendTo("/topic/online")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        messagingTemplate.convertAndSend("/topic/online", chatMessage);
         return chatMessage;
     }
 
-    @MessageMapping("/chat.addUser")
-    @SendTo("/topic/public")
-    public ChatMessage addUser (@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor){
-        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+
+    @MessageMapping("/chat.sendMessageV1")
+    public ChatMessage sendMessageV1(@Payload ChatMessage chatMessage) {
+        String dynamicSession = chatMessage.getSessionID();
+        String destination = "/direct/" + dynamicSession;
+        messagingTemplate.convertAndSend(destination, chatMessage);
         return chatMessage;
     }
 }
